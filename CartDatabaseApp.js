753df464dc55318f8f5e94cab05765f117161296
Promigo Ventures <promigoventures@gmail.com>
2025-08-02 15:09:08 +0100

    Added all files

---
+++ CartDatabaseApp.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL: "https://playground-894ce-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const moviesInDB = ref(database, "movies");
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-btn");
+
+addButtonEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value;
+  push(moviesInDB, inputValue);
+  clearInputField();
+  appendItemToShoppingListEl(inputValue);
+});
